e28b8e9bee8217e747a6ca16809934255ea31117
italo Martins Tomaz Rocha <082170012@ftt.cefsa.edu.br>
2022-01-19 19:22:58 -0300

    teste publish docker

---
+++ libras4All-web/Controllers/EditarController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libras4All_web.Controllers
+{
+    public class EditarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ libras4All-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace libras4All_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
