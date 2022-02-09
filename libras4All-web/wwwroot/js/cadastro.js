a9cef3c58870b1b4da7405ba391b0bb9027aaf2f
milenaff <mih.ferreiradepaula@gmail.com>
2022-02-09 19:46:49 -0300

    Cadastro Usuário API-FRONT

--- libras4All-web/wwwroot/js/cadastro.js
+++ libras4All-web/wwwroot/js/cadastro.js
@@ -1,11 +1,11 @@
 function cadastrarUsuario() {
-    //var url = "https://libras4all.herokuapp.com/api/usuario/criar";
-    var url = "http://localhost:9090/api/usuario/criar";
+    var url = "https://libras4all.herokuapp.com/api/usuario/criar";
+    //var url = "http://localhost:9090/api/usuario/criar";
 
     var nomeUsuario = $('#exampleDropdownFormNome1').val();
     var loginUsuario = $('#exampleDropdownFormEmail1').val();
     var senhaUsuario = $('#exampleDropdownFormPassword1').val();
-    debugger;
+   
     $.ajax({
         type: "POST",
         url: url,
@@ -13,8 +13,8 @@ function cadastrarUsuario() {
         cache: false
     })
         .done(function (data) {
-            alert("Usu�rio cadastrado com sucesso");
-            //window.location = "https://libras4all-web.herokuapp.com/Login";
+            alert("Usuário cadastrado com sucesso");
+            window.location.href = "https://libras4all-web.herokuapp.com/Login"; // não está redirecionando
 
         })
 
