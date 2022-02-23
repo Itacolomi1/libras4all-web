02dab1be30e8425e1e77555649c0425d440bcedf
milenaff <mih.ferreiradepaula@gmail.com>
2022-02-23 14:06:28 -0300

    Lista alunos ainda não funcionando o data table

--- libras4All-web/wwwroot/js/listaAlunos.js
+++ libras4All-web/wwwroot/js/listaAlunos.js
@@ -1,20 +1,20 @@
 ﻿$(document).ready(function () {
 
 
-    var token = localstorage.getitem('user_token').replaceall("\"", "");
-    var id = localstorage.getitem('user_id');
+    //var token = localstorage.getitem('user_token').replaceall("\"", "");
+    //var id = localstorage.getitem('user_id');
     debugger;
     $('#tabelaAlunos').DataTable({
         dom: "Bfrtip",
         ajax: {
 
-           url: "https://libras4all.herokuapp.com/api/usuario/obteralunosporprofessor/"+ id,
-            //url: "https://libras4all.herokuapp.com/api/usuario/obteralunosporprofessor/62047eea4ec5754220485566",
+           //url: "https://libras4all.herokuapp.com/api/usuario/obteralunosporprofessor/"+ id,
+            url: "https://libras4all.herokuapp.com/api/usuario/obteralunosporprofessor/62047eea4ec5754220485566",
             type: 'GET',
-            headers: {
-                authorization: 'bearer ' + token
-            },
-            "datasrc": '',
+            //headers: {
+            //    authorization: 'bearer ' + token
+            //},
+            //"datasrc": '',
         },
         columns: [
 
