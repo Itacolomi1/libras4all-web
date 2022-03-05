c5ff6ab958ef3c2e89f9c53686355530f528278f
milenaff <mih.ferreiradepaula@gmail.com>
2022-03-04 23:30:31 -0300

    Home alunos FINALIZADA

--- libras4All-web/wwwroot/js/homeAluno.js
+++ libras4All-web/wwwroot/js/homeAluno.js
@@ -1,12 +1,13 @@
 ﻿var array = [];
 
 var idAluno;
+var idSala;
 
 var token = localStorage.getItem('user_token').replaceAll("\"", "");
 var id = localStorage.getItem('user_id').replaceAll("\"", "");
 
-//var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWJlOWI4MmQ1M2EzMDAxNmEwYjU2ZSIsImlhdCI6MTY0NjMzNTYwNH0.zo-YM5pZn7WugFNNeSsm28TsW1Df1jmWO9Rs7LCQT7Y';
-//var id = '621be9b82d53a30016a0b56e';
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWJlOWI4MmQ1M2EzMDAxNmEwYjU2ZSIsImlhdCI6MTY0NjQyMzE4OH0.jlc0iaSFUx39GxJnhiYLKTQe9oO1o6tra50xICr-iDQ';
+var id = '621be9b82d53a30016a0b56e';
 
 async function AdicionaLista(alunos, token) {
 
@@ -60,8 +61,6 @@ async function CarregaDadosAlunoSelecionado(id) {
 
 async function CarregaDados() {
 
-    var token = localStorage.getItem('user_token').replaceAll("\"", "");
-
     var url = "https://libras4all.herokuapp.com/api/usuario/obterAlunosPorProfessor/" + id;
 
     await $.ajax({
@@ -107,20 +106,96 @@ async function montaTabela() {
     });
 
 }
-function montaTabelaSalas(dado) {
+async function montaTabelaSalas(dado) {
+    debugger;
 
-
-    $('#tabelaSalas').DataTable({
+    await $('#tabelaSalas').DataTable({
         dom: "Bfrtip",
         data: dado,
         destroy: true,
         columns: [
             { data: "descricao" },
+            {data: "tipoJogo"},
         ],
+    });
+
+    var tab = $('#tabelaSalas').DataTable();
+
+    $('#tabelaSalas tbody').on('click', 'tr', function () {
+
+        idSala = tab.row(this).data()._id;
+        BuscaAcertosErros(idAluno, idSala);
+
     });
 }
 
 $(document).ready(function () {
 
     CarregaDados();
+    
 });
+
+async function BuscaAcertosErros(id_aluno, id_sala) {    
+
+    var url = "https://libras4all.herokuapp.com/api/historico/quantidadePorAluno/" + id_sala + "/" + id_aluno;
+
+    await $.ajax({
+        type: "GET",
+        url: url,
+        headers: {
+            authorization: 'bearer ' + token
+        },
+        success: function (retorno) {
+
+           Grafico(retorno);           
+           
+        }
+    });
+}
+
+ function Grafico(dados) {
+
+    const propertyValues = Object.values(dados);
+
+     console.log(propertyValues);
+     debugger;
+     let ctx = document.getElementById('primeiroGrafico');
+    let myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+           
+            labels: ['Acertos', 'Erros'],
+            datasets: [{
+                // Legenda geral
+                label: '',               
+                data: propertyValues,            
+                
+                backgroundColor: [
+                    'rgba(0,191,255)',
+                    'rgba(255,0,0)',
+                    
+                ],
+                
+                borderColor: [
+                    'rgba(0,0,139)',
+                    'rgba(139,0,0)',                   
+                ],
+                // Define a espessura da borda dos retângulos
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+    });
+    
+}
