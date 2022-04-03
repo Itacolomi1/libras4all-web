ebbf1d36954dca4942a2a173d19916c6b20e3349
milenaff <mih.ferreiradepaula@gmail.com>
2022-04-03 14:33:50 -0300

    Acrescimo do campo data de nascimento e alteração do icone

--- libras4All-web/wwwroot/js/cadastro.js
+++ libras4All-web/wwwroot/js/cadastro.js
@@ -1,8 +1,13 @@
 function cadastrarUsuario() {
     var url = "https://libras4all.herokuapp.com/api/professor";
+    //var url = "http://localhost:9090/api/professor";
+    
     var nome = $('#exampleDropdownFormNome1').val();
     var email = $('#exampleDropdownFormEmail1').val();
-    var senha = $('#exampleDropdownFormPassword1').val();
+    var senha = $('#exampleDropdownFormPassword1').val();   
+    var dataNascimento = document.getElementById('exampleDropdownFormDate1').value;
+    debugger;
+
     $.ajax({
         type: "POST",
         url: url,
@@ -14,7 +19,8 @@ function cadastrarUsuario() {
             window.location = "https://libras4all-web.herokuapp.com/Login";
         })
         .fail(function (XMLHttpRequest, textStatus, errorThrown) {
-            console.log(errorThrown);
+            debugger;
+            console.log(error);
             alert("Erro ao cadastrar usuário.");
         });
 }
