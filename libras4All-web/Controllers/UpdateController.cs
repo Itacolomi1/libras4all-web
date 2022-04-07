eefcaa803543c1b1f74b14710d357c7bf20e3142
milenaff <mih.ferreiradepaula@gmail.com>
2022-04-07 19:47:47 -0300

    páginas para recuperar senha

---
+++ libras4All-web/Controllers/UpdateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libras4All_web.Controllers
+{
+    public class UpdateController:Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
