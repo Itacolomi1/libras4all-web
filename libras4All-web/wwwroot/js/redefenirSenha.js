8d87ae2fffa1f8a171e1d0aa3dff114043f2e99f
milenaff <mih.ferreiradepaula@gmail.com>
2022-04-07 21:23:27 -0300

    Estilização das paginas de redefinição de senha

--- libras4All-web/wwwroot/js/redefenirSenha.js
+++ libras4All-web/wwwroot/js/redefenirSenha.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+
+    $("#eye").mousedown(function () {
+        $("#exampleDropdownFormPassword1").attr("type", "text");
+    });
+
+    $("#eye").mouseup(function () {
+        $("#exampleDropdownFormPassword1").attr("type", "password");
+    });
+    $("#eye2").mousedown(function () {
+        $("#exampleDropdownFormPassword2").attr("type", "text");
+    });
+
+    $("#eye2").mouseup(function () {
+        $("#exampleDropdownFormPassword2").attr("type", "password");
+    });
+
+});
