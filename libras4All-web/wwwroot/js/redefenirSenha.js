3bb1413cc0381e145cbc9ce705460bab20dc00b8
Patricia Castro <48774696+patrcastro@users.noreply.github.com>
2022-04-08 23:36:12 -0300

    Criação da redefinição de senha

--- libras4All-web/wwwroot/js/redefenirSenha.js
+++ libras4All-web/wwwroot/js/redefenirSenha.js
@@ -1,5 +1,33 @@
-﻿$(document).ready(function () {
+﻿var url = "https://libras4all.herokuapp.com/api/email";
+var urlString = new URL(window.location.href);
+var req = urlString.searchParams.get("req");
 
+
+$(document).ready(function () {
+    processarCampos();
+    validarRequisicao();
+});
+
+function validarRequisicao() {
+    $.ajax({
+        type: "GET",
+        url: url + "/validar/" + req,
+        cache: false
+    })
+        .done(function (data) {
+            processarRedefinicao();
+        })
+        .fail(function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Link incorreto ou expirado!");
+        });
+}
+
+function processarRedefinicao() {
+    document.getElementById("redefinicao").style.display = 'block';
+    document.getElementById("divSalvar").style.display = 'block';
+}
+
+function processarCampos() {
     $("#eye").mousedown(function () {
         $("#exampleDropdownFormPassword1").attr("type", "text");
     });
@@ -14,19 +42,41 @@
     $("#eye2").mouseup(function () {
         $("#exampleDropdownFormPassword2").attr("type", "password");
     });
-
-});
-
-
-function validaSenhas() {
-
-    var senha1 = $('exampleDropdownFormPassword1').text;
-    var senha2 = $('exampleDropdownFormPassword2').text;
-
-    if (senha1 == senha2)
-        SalvaNovaSenha();
 }
 
-function SalvaNovaSenha() {
+function redefinirSenha() {
+    var txtSenha = $('#exampleDropdownFormPassword1').val();
+    var txtConfirmarSenha = $('#exampleDropdownFormPassword2').val();
 
+    if (txtSenha == txtConfirmarSenha) {
+        atualizarSenha();
+    }
+    else {
+        alert("As senhas precisam ser a mesma!");
+    }
+}
+
+function atualizarSenha() {
+    var txtSenha = $('#exampleDropdownFormPassword1').val();
+
+    console.log(req)
+    console.log(txtSenha)
+
+    $.ajax({
+        type: "PUT",
+        url: url + "/atualizar",
+        data:
+        {
+            idHistorico: req,
+            novaSenha: txtSenha
+        },
+        cache: false
+    })
+        .done(function (data) {
+            alert("Senha redefinida com sucesso!");
+            window.location = "https://libras4all-web.herokuapp.com/Login";
+        })
+        .fail(function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Erro ao redefinir a senha!");
+        });
 }
