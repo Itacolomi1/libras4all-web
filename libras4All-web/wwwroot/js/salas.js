5998a835fc381ebe5c27b1d586bfd81c23494925
milenaff <mih.ferreiradepaula@gmail.com>
2022-04-30 11:03:50 -0300

    Bug sala inativa

--- libras4All-web/wwwroot/js/salas.js
+++ libras4All-web/wwwroot/js/salas.js
@@ -19,54 +19,112 @@ $(document).ready(function () {
 
     var url = "https://libras4all.herokuapp.com/api/sala/listarSalasProfessor/"
 
-    $('#tbSalas').DataTable({
-        dom: "Bfrtip",
-        "columnDefs": [
-            { "width": "50%", "targets": 1 },
+    $.ajax({
+        type: "GET",
+        url: url + id,
+        cache: false,
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    })
+        .done(function (data) {
+            console.log(data);
+            debugger;
 
-        ],
-        "pageLength": 3,
-        "language": {
-            "paginate": {
-                "previous": "Anterior",
-                "next": "Próximo"
-            },
-            "emptyTable": "Sem dados",
-            "search": "Pesquisar: ",
-            "info": "_PAGE_ de _PAGES_"
-        },
-        ajax: {
-            url: url + id,
-            type: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + token
-            },
-            "dataSrc": '',
-        },
-        columns: [
-            { data: "codigo" },
-            { data: "descricao" },
-            {
-                data: function (data, type, row) {
-                    var strStatus;
-                    if (data.status)
-                        strStatus = 'Ativa'
-                    else
-                        strStatus = 'Inativa'
-                    return strStatus;
-                }
-            },
-            { data: "tipoJogo" },
-            {
-                data: function (data, type, row) {
-                    return '<a href="Visualizar?Id=' + data._id + '">' +
-                        '<img class="btnVisualizar" style="width: 30px;" src="./images/visualizar.png"></img>' +
-                        '</a> ' +
-                        '<a href="Editar?Id=' + data._id + '">' +
-                        '<img style="width: 30px;" src="./images/editar.png"></img>' +
-                        '</a> ' ;
-                }
-            },
-        ],
-    });
+            $('#tbSalas').DataTable({
+                dom: "Bfrtip",
+                "columnDefs": [
+                    { "width": "50%", "targets": 1 },
+
+                ],
+                "pageLength": 3,
+                "language": {
+                    "paginate": {
+                        "previous": "Anterior",
+                        "next": "Próximo"
+                    },
+                    "emptyTable": "Sem dados",
+                    "search": "Pesquisar: ",
+                    "info": "_PAGE_ de _PAGES_"
+                },
+                data: data,
+                columns: [
+                    { data: "codigo" },
+                    { data: "descricao" },
+                    {
+                        data: function (data, type, row) {
+                            var strStatus;
+                            console.log(data.status)
+                            if (data.status=='true')
+                                strStatus = 'Ativa'
+                            else
+                                strStatus = 'Inativa'
+                            return strStatus;
+                        }
+                    },
+                    { data: "tipoJogo" },
+                    {
+                        data: function (data, type, row) {
+                            return '<a href="Visualizar?Id=' + data._id + '">' +
+                                '<img class="btnVisualizar" style="width: 30px;" src="./images/visualizar.png"></img>' +
+                                '</a> ' +
+                                '<a href="Editar?Id=' + data._id + '">' +
+                                '<img style="width: 30px;" src="./images/editar.png"></img>' +
+                                '</a> ';
+                        }
+                    },
+                ],
+            });
+        });
+
+    //$('#tbSalas').DataTable({
+    //    dom: "Bfrtip",
+    //    "columnDefs": [
+    //        { "width": "50%", "targets": 1 },
+
+    //    ],
+    //    "pageLength": 3,
+    //    "language": {
+    //        "paginate": {
+    //            "previous": "Anterior",
+    //            "next": "Próximo"
+    //        },
+    //        "emptyTable": "Sem dados",
+    //        "search": "Pesquisar: ",
+    //        "info": "_PAGE_ de _PAGES_"
+    //    },
+    //    ajax: {
+    //        url: url + id,
+    //        type: 'GET',
+    //        headers: {
+    //            Authorization: 'Bearer ' + token
+    //        },
+    //        "dataSrc": '',
+    //    },
+    //    columns: [
+    //        { data: "codigo" },
+    //        { data: "descricao" },
+    //        {
+    //            data: function (data, type, row) {
+    //                var strStatus;
+    //                if (data.status)
+    //                    strStatus = 'Ativa'
+    //                else
+    //                    strStatus = 'Inativa'
+    //                return strStatus;
+    //            }
+    //        },
+    //        { data: "tipoJogo" },
+    //        {
+    //            data: function (data, type, row) {
+    //                return '<a href="Visualizar?Id=' + data._id + '">' +
+    //                    '<img class="btnVisualizar" style="width: 30px;" src="./images/visualizar.png"></img>' +
+    //                    '</a> ' +
+    //                    '<a href="Editar?Id=' + data._id + '">' +
+    //                    '<img style="width: 30px;" src="./images/editar.png"></img>' +
+    //                    '</a> ' ;
+    //            }
+    //        },
+    //    ],
+    //});
 });
